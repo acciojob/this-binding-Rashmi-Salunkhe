88ee1e7f8144bb1e4d6ca5db9b9f64f0ce3a4778
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-09-20 11:30:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-//your code here
 // Implementing myCall
 Function.prototype.myCall = function(thisContext, ...args) {
-  
-  };
-  
-  // Implementing myApply
-  Function.prototype.myApply = function(thisContext, args) {
-    
-  
-  };
-  
-  // Implementing myBind
-  Function.prototype.myBind = function(thisContext, ...args) {
-    
-  };
-  
-  module.exports = Function.prototype;
+    thisContext.fn = this; // Assign the function to the context
+    const result = thisContext.fn(...args); // Call the function with the provided arguments
+    delete thisContext.fn; // Clean up the temporary property
+    return result;
+};
+
+// Implementing myApply
+Function.prototype.myApply = function(thisContext, args) {
+    thisContext.fn = this; // Assign the function to the context
+    const result = thisContext.fn(...args); // Spread the args array as arguments
+    delete thisContext.fn; // Clean up the temporary property
+    return result;
+};
+
+// Implementing myBind
+Function.prototype.myBind = function(thisContext, ...args) {
+    const fn = this; // Store the original function reference
+    return function(...newArgs) {
+        return fn.myCall(thisContext, ...args, ...newArgs); // Combine old and new arguments
+    };
+};
+
+module.exports = Function.prototype;
